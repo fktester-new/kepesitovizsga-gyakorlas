f495819a5621687b18d8d3205276a2512edd146d
fktester <kofe16@gmail.com>
2021-04-25 16:19:17 +0200

    SumOfDigits

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/sumofdigits/SumOfDigits.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/sumofdigits/SumOfDigits.java
@@ -1,4 +1,23 @@
 package hu.nive.ujratervezes.kepesitovizsga.sumofdigits;
 
+import java.util.Random;
+
 public class SumOfDigits {
+
+    Random random = new Random();
+
+    public int getSumOfDigits(Random random) {
+        int number = random.nextInt();
+        number = Math.abs(number);
+
+        while (number > 9) {
+            int sumOfDigits = 0;
+            while (number > 0) {
+                sumOfDigits += number % 10;
+                number /= 10;
+            }
+            number = sumOfDigits;
+        }
+        return number;
+    }
 }
